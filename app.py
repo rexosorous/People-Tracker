9a23a9c5a45a47b033c889824dfd6067d8114f57
J-Zach Loke <jzloke@gmail.com>
2021-04-26 23:16:47 -0700

    fixed an issue where sensor data could not be read

--- app.py
+++ app.py
@@ -86,13 +86,16 @@ class Tracker:
             There is an allotted time to trip the second sensor before it times out and
             "thinks" the person has turned back.
         '''
+        print('tripped')
         date = utils.iso_date() # iso 8601 format (yyyy-mm-dd) because we're not savages
         start_time = utils.unix_time() # used for timeout
         while utils.unix_time() < start_time + self.timeout:  # poll the sensors until timeout
             if sensor.distance < floor:
+                print('adding to db')
                 self.db.increment(db_field, date)
                 sensor.wait_for_out_of_range()
                 return
+            sleep(0.001)
 
 
 
@@ -106,9 +109,12 @@ class Tracker:
             3. both sensors are tripped which indicates that someone is moving slowly through the doorway
             4. no sensors are tripped and there is no movement/people
         '''
+        print('running...')
         while True:
             ingress_distance = self.ingress_sensor.distance
             egress_distance = self.egress_sensor.distance
+            sleep(0.001) # for some reason, we need these sleep statements anytime we want to capture readings from the sensors
+                         # we can get this to work by replacing sleeps with print statements if so desired
             if ingress_distance < self.ingress_floor and egress_distance < self.egress_floor:
                 # both sensors are tripped usually when someone is moving slowly through the doorway
                 # we need to ignore input from the sensors (by sleeping) for a short while to allow
